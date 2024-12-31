9f300785c05a66af2a28ff05266f81e3d684e308
KY64 <taurus99@protonmail.com>
2024-12-31 23:46:17 +0700

    chore: remove neovim and add doc on setup touchpad

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@ sudo dnf install -y \
 		xorg-x11-server-Xorg \
 		python3-nodeenv \
 		podman \
-		neovim \
 		git \
 		i3 \
 		st \
@@ -30,3 +29,19 @@ echo "alias n='source ~/node/bin/activate'" >> ~/.bashrc
 # Execute i3 on 'startx' command
 
 echo "exec i3" >> $HOME/.xinitrc
+
+# Next instruction:
+
+# 1. Touchpad
+# If touchpad is not working, try check whether libinput is already installed
+# Check whether touchpad is detected by running the following command
+# 	xinput list
+# Check whether 'Tap to click' is already enabled by running
+#	xinput list-props <device-id>
+# The device ID can be known when running 'xinput list' there will be something like 'id=13' 
+# on the right columns
+#
+# Find "Tapping Enabled" and the most right value should be 1, if not then we can set the value like following
+# 	sudo xinput set-prop <device-id> <props-id> 1
+# The props ID is a number inside a bracket, for example "libinput Tapping Enabled (319)" has props ID 319
+#
